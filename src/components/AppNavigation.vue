e466a9d1fbc3ef783542e6f6be04742a3fd48de7
Thalles <thallesgabriel1307@gmail.com>
2020-12-02 14:01:23 -0300

    Initial Coding

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-tollbar app color="brown darken-4" dark>
+        <v-tollbar-side-icon></v-tollbar-side-icon>
+        <v-tollbar-title>{{appTitle}}</v-tollbar-title>
+        <v-btn flat>Menu</v-btn>    
+        <v-spacer></v-spacer>
+        <v-btn flat>SIGN IN</v-btn>
+        <v-btn color="brown lighten-3">JOIN</v-btn>
+    </v-tollbar>    
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation'
+};
+</script>
+
+<style scoped>
+
+</style>
