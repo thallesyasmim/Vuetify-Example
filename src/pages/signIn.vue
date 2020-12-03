b2049a87750bbdc32717126000967329aec1111e
Thalles <thallesgabriel1307@gmail.com>
2020-12-03 13:25:58 -0300

    New pages

---
+++ src/pages/signIn.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container fluid>
+        <v-layout>
+            <h1>Sign-in</h1>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'Signin'   
+    }
+</script>
+
+<style scoped>
+</style>
