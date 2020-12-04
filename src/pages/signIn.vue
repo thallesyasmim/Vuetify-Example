c278abf9b6e1a54145ab72023cad5ab13b16af74
Thalles <thallesgabriel1307@gmail.com>
2020-12-04 13:01:12 -0300

    Create Login Form

--- src/pages/signIn.vue
+++ src/pages/signIn.vue
@@ -1,15 +1,88 @@
 <template>
-    <v-container fluid>
-        <v-layout>
-            <h1>Sign-in</h1>
+    <v-container fill-height>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+                <v-card class="elevation-12">
+                    <v-toolbar dark color="brown">
+                        <v-toolbar-title class="font-weight-black white--text text-xs-center">SIGN IN FORM</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form ref="form" v-model="valid" lazy-validation>
+                            <v-text-field 
+                                prepend-icon="mdi-account"
+                                name="email" 
+                                label="Email" 
+                                type="email"
+                                v-model="email" 
+                                :rules="emailRules" 
+                                required>
+                            </v-text-field>
+                            <v-text-field 
+                                prepend-icon="mdi-lock"
+                                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                                name="password" 
+                                label="Password" 
+                                id="password"
+                                :type="show1 ? 'text' : 'password'"
+                                hint="At least 6 characters"
+                                required 
+                                v-model="password"
+                                :rules="passwordRules"
+                                @click:append="show1 = !show1"
+                                >
+                            </v-text-field>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="brown" :disabled="!valid" @click="submit" class="white--text mb-1 mr-2">Login</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
-    export default {
-        name: 'Signin'   
+    import firebase from 'firebase';
+
+export default {
+    name: 'Signin',
+    data() {
+        return {
+            valid: false,
+            email: '',
+            password: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+/.test(v) || 'E-mail must be valid'
+            ],
+            passwordRules: [
+                v => !!v || 'Password is required',
+                v =>
+                    v.length >= 6 ||
+                    'Password must be greater than 6 characters'
+            ],
+            show1: false
+        };
+    },
+    methods: {
+         async submit() {
+            if (this.$refs.form.validate()) {
+                try {
+                    await this.userLogin();
+                    this.$router.push('welcome');
+                }
+                catch(error) {
+                    console.log(error);
+                }
+            }
+        },
+        userLogin() {
+            firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+        }
     }
+};
 </script>
 
 <style scoped>
