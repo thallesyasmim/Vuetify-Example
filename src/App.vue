4572f13e9d703cc74cc8231fd2733ec88b8c7d26
Thalles <thallesgabriel1307@gmail.com>
2020-12-04 14:21:45 -0300

    Navigation Validation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app>
     <v-content transition="slide-x-transition">
-      <AppNavigation />
+      <AppNavigation v-if="this.$route.path.substring(1, 10) !== 'welcome'"/>
+      <WelcomeNavigation v-if="this.$route.path.substring(1, 10) === 'welcome'"/>
       <router-view></router-view> <!-- Visualization Routes -->
       <Footer />
     </v-content>
@@ -10,12 +11,14 @@
 
 <script>
   import AppNavigation from './components/AppNavigation.vue';
+  import WelcomeNavigation from './components/welcomeNavigation.vue';
   import Footer from './components/Footer.vue';
 
   export default {
     name: 'App',
     components: {
       AppNavigation,
+      WelcomeNavigation,
       Footer
     }
   }
